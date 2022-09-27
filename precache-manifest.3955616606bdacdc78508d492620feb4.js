adc8e6cb352495ed576c60a16501cbd3e9da746a
moviesharing <109523229+moviesharing@users.noreply.github.com>
2022-09-27 14:17:23 +0800

    Add files via upload

---
+++ precache-manifest.3955616606bdacdc78508d492620feb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf491256bcb240af7c43dd462936fef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ad5877df8e8e7ea0a6d",
+    "url": "/static/css/main.6ed30cd0.chunk.css"
+  },
+  {
+    "revision": "17fdc4bd88018f466f49",
+    "url": "/static/js/2.469fcbdf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.469fcbdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad5877df8e8e7ea0a6d",
+    "url": "/static/js/main.ad30faaf.chunk.js"
+  },
+  {
+    "revision": "83acbac32e5258769ab2",
+    "url": "/static/js/runtime-main.0d3282c9.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/static/media/city.91c109fc.jpg"
+  }
+]);
